6cd1eb5ab2909d6fc3571a4c054ccb2ecae5d573
Anis Ahmad <anisniit@gmail.com>
2013-10-31 00:27:35 +0600

    Project added in new git repo

---
+++ Db.php
@@ -0,0 +1,178 @@
+<?php
+/**
+ * Db
+ *
+ * A simple wrapper for PDO.
+ * Inspired by the sweet PDO wrapper from http://www.fractalizer.ru
+ *
+ * @author  Anis uddin Ahmad <anisniit@gmail.com>
+ * @link    http://www.fractalizer.ru/frpost_120/php-pdo-wrapping-and-making-sweet/
+ * @link    http://ajaxray.com
+ */
+class Db
+{
+    private static $_pdoObject = null;
+
+    protected static $_fetchMode = PDO::FETCH_ASSOC;
+    protected static $_connectionStr = null;
+    protected static $_driverOptions = array();
+
+    private static $_username = null;
+    private static $_password = null;
+
+    /**
+     * Set connection information
+     *
+     * @example    Db::setConnectionInfo('basecamp','dbuser', 'password', 'mysql', 'http://mysql.abcd.com');
+     */
+    public static function setConnectionInfo($schema, $username = null, $password = null, $database = 'mysql', $hostname = 'localhost')
+    {
+        if($database == 'mysql') {
+            self::$_connectionStr = "mysql:dbname=$schema;host=$hostname";
+            self::$_username      = $username;
+            self::$_password      = $password;
+        } else if($database == 'sqlite'){
+            // For sqlite, $schema is the file path
+            self::$_connectionStr = "sqlite:$schema";
+        }
+
+        // Making the connection blank
+        // Will connect with provided info on next query execution
+        self::$_pdoObject = null;
+    }
+
+    /**
+     * Execute a statement and returns number of effected rows
+     *
+     * Should be used for query which doesn't return resultset
+     *
+     * @param   string  $sql    SQL statement
+     * @param   array   $params A single value or an array of values
+     * @return  integer number of effected rows
+     */
+    public static function execute($sql, $params = array())
+    {
+        $statement = self::_query($sql, $params);
+        return $statement->rowCount();
+    }
+
+    /**
+     * Execute a statement and returns a single value
+     *
+     * @param   string  $sql    SQL statement
+     * @param   array   $params A single value or an array of values
+     * @return  mixed
+     */
+    public static function getValue($sql, $params = array())
+    {
+        $statement = self::_query($sql, $params);
+        return $statement->fetchColumn(0);
+    }
+
+    /**
+     * Execute a statement and returns the first row
+     *
+     * @param   string  $sql    SQL statement
+     * @param   array   $params A single value or an array of values
+     * @return  array   A result row
+     */
+    public static function getRow($sql, $params = array())
+    {
+        $statement = self::_query($sql, $params);
+        return $statement->fetch(self::$_fetchMode);
+    }
+
+    /**
+     * Execute a statement and returns row(s) as 2D array
+     *
+     * @param   string  $sql    SQL statement
+     * @param   array   $params A single value or an array of values
+     * @return  array   Result rows
+     */
+    public static function getResult($sql, $params = array())
+    {
+        $statement = self::_query($sql, $params);
+        return $statement->fetchAll(self::$_fetchMode);
+    }
+
+    public static function getLastInsertId($sequenceName = "")
+    {
+        return self::$_pdoObject->lastInsertId($sequenceName);
+    }
+
+    public static function setFetchMode($fetchMode)
+    {
+        self::_connect();
+        self::$_fetchMode = $fetchMode;
+    }
+
+    public static function getPDOObject()
+    {
+        self::_connect();
+        return self::$_pdoObject;
+    }
+
+    public static function beginTransaction()
+    {
+        self::_connect();
+        self::$_pdoObject->beginTransaction();
+    }
+
+    public static function commitTransaction()
+    {
+        self::$_pdoObject->commit();
+    }
+
+    public static function rollbackTransaction()
+    {
+        self::$_pdoObject->rollBack();
+    }
+
+    public static function setDriverOptions(array $options)
+    {
+        self::$_driverOptions = $options;
+    }
+
+    private static function _connect()
+    {
+        if(self::$_pdoObject != null){
+            return;
+        }
+        
+        if(self::$_connectionStr == null) {
+            throw new PDOException('Connection information is empty. Use Db::setConnectionInfo to set them.');
+        }
+
+        self::$_pdoObject = new PDO(self::$_connectionStr, self::$_username, self::$_password, self::$_driverOptions);
+    }
+
+    /**
+     * Prepare and returns a PDOStatement
+     *
+     * @param   string  $sql SQL statement
+     * @param   array   $params Parameters. A single value or an array of values
+     * @return  PDOStatement
+     */
+    private static function _query($sql, $params = array())
+    {
+        if(self::$_pdoObject == null) {
+            self::_connect();
+        }
+
+        $statement = self::$_pdoObject->prepare($sql, self::$_driverOptions);
+
+        if (! $statement) {
+            $errorInfo = self::$_pdoObject->errorInfo();
+            throw new PDOException("Database error [{$errorInfo[0]}]: {$errorInfo[2]}, driver error code is $errorInfo[1]");
+        }
+
+        $paramsConverted = (is_array($params) ? ($params) : (array ($params )));
+
+        if ((! $statement->execute($paramsConverted)) || ($statement->errorCode() != '00000')) {
+            $errorInfo = $statement->errorInfo();
+            throw new PDOException("Database error [{$errorInfo[0]}]: {$errorInfo[2]}, driver error code is $errorInfo[1]");
+        }
+
+        return $statement;
+    }
+}
---
+++ DbExample.php
@@ -0,0 +1,42 @@
+<?php
+require_once 'Db.php';
+
+// Set your connection info here
+Db::setConnectionInfo('test', 'root', '');
+
+// Creating table for our testing
+$query = "CREATE TABLE IF NOT EXISTS users(
+			id INTEGER PRIMARY KEY,
+			username varchar(100) NOT NULL,
+			password varchar(100) NOT NULL,
+			status   char(10)
+		);";
+		
+Db::execute($query);
+
+// Let's create some data
+$data = array(
+	array('name'=>'anis', 'pass'=>'ajaxray'),
+	array('name'=>'emran', 'pass'=>'phpfour'),
+	array('name'=>'raju', 'pass'=>'stylephp'),
+);
+
+foreach($data as $user){
+	Db::execute('INSERT INTO users(username, password) VALUES(:name, :pass)', $user);
+}
+
+// Retriving a field
+$name =  Db::getValue('SELECT username FROM users WHERE id = ?', 2);
+echo 'Name of 2nd user : ' . $name . "<br />";
+
+// Retriving a row
+$row =  Db::getRow('SELECT * FROM users WHERE id = ?', 1);
+echo 'Row of 1st user : <pre>';
+print_r($row);
+echo "</pre> <br />";
+
+// Retriving resultset
+$result =  Db::getResult('SELECT * FROM users limit 2');
+echo '1st 3 users : <pre>';
+print_r($result);
+echo "</pre> <br />";
